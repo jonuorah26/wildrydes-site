bbe8977598674de6b6da79198dd4058582787570
jonuorah26 <99527221+jonuorah26@users.noreply.github.com>
2022-11-26 20:49:12 -0600

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-1_LWzxfaHja',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   'p2qkd95hgd4r9bh0h4hlbn3tk',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
